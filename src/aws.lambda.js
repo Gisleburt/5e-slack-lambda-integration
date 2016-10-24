29c0f2d1710d5f41966ca3dd8f2c58a49df091f2
Daniel Mason <danielm@moo.com>
2016-10-24 16:54:39 +0100

    Multiple commands taking shape

--- src/aws.lambda.js
+++ src/aws.lambda.js
@@ -4,6 +4,33 @@ const AWS = require('aws-sdk');
 const qs = require('querystring');
 
 const token = '===TOKEN===';
+const slashCommand = '/5e';
+
+const commands = {
+    help: {
+        description: 'Lists available commands',
+        call(additionalData, params) {
+            return {
+                response_type: 'in_channel',
+                text: 'The following commands are currently available',
+                attachments: {
+                    text: Object.keys(commands).reduce((data, current) => (
+                      `${data}\\n${slashCommand} ${current} - ${commands[current].description}`
+                    ), '')
+                },
+            };
+        },
+    },
+    roll: {
+        description: `Roll a dice. Eg: ${slashCommand} roll d20`,
+        call(additionalData, params) {
+            return {
+                response_type: 'in_channel',
+                text: 'Ohps, dropped my dice!',
+            };
+        },
+    },
+};
 
 function processEvent(event, callback) {
     const params = qs.parse(event.body);
@@ -18,9 +45,30 @@ function processEvent(event, callback) {
     const channel = params.channel_name;
     const commandText = params.text;
 
-    callback(null, `${user} invoked ${command} in ${channel} with the following text: ${commandText}`);
+    // Get the first word after slashCommand
+    const desiredCommand = getFirstWord(commandText);
+
+    if(desiredCommand && commands.hasOwnProperty(desiredCommand) && commands[desiredCommand].hasOwnProperty('call')) {
+        callback(null, commands[desiredCommand].call());
+        return;
+    }
+
+    callback(`I don't understand the command "${commandText}". Try "${slashCommand} help"`);
 }
 
+function getFirstWord(string) {
+    if (!string) {
+        return '';
+    }
+    return string.split(' ')[0];
+}
+
+function removeFirstWord(string) {
+    if (!string) {
+        return '';
+    }
+    return string.substr(string.indexOf(" ") + 1);
+}
 
 exports.handler = (event, context, callback) => {
     const done = (err, res) => callback(null, {
